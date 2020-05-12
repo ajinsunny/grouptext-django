95a301ec645e747b0159288fb8585ee384164758
Samin Akter <syakter@ucsc.edu>
2020-05-12 10:32:43 -0700

    created models

--- codes/grouptext/models.py
+++ codes/grouptext/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class TextGroup(models.Model):
+    group_name = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class TextGroupMember(models.Model):
+    text_group = models.ForeignKey(TextGroup, on_delete=models.CASCADE)
+    member_name = models.CharField(max_length=64)
+    member_phone = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class TextQuestion(models.Model):
+    text_group = models.ForeignKey(TextGroup, on_delete=models.CASCADE)
+    question = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+    sent = models.BooleanField(default=False)
