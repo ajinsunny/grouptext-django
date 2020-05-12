7b6badf6cc156a28d993b088c52188fda3024474
Samin Akter <syakter@ucsc.edu>
2020-05-12 12:41:25 -0700

    registered models with admin site and migrated

--- codes/grouptext/admin.py
+++ codes/grouptext/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TextGroup, TextGroupMember, TextQuestion
 
 # Register your models here.
+admin.site.register(TextGroup)
+admin.site.register(TextGroupMember)
+admin.site.register(TextQuestion)
--- codes/grouptext/models.py
+++ codes/grouptext/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class TextGroup(models.Model):
     group_name = models.CharField(max_length=64)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    
 class TextGroupMember(models.Model):
     text_group = models.ForeignKey(TextGroup, on_delete=models.CASCADE)
     member_name = models.CharField(max_length=64)
