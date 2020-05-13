8a77f4b90a7268bbcc6f7828e77b46814bf98d8d
Aaron Rosen <aaronorosen@gmail.com>
2020-05-13 12:56:42 -0700

    database integration

--- codes/web/settings.py
+++ codes/web/settings.py
@@ -74,14 +74,16 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'USER': 'db_user',
+        'HOST': 'db',
+        'PORT': '5432',
+        'PASSWORD': 'db_pass'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
