1a0c3f214dba1e7047f8e1ec58121d9355b38b8c
Aaron Rosen <aaronorosen@gmail.com>
2020-05-24 20:58:55 -0700

    Fix cors configuration issue

--- codes/web/settings.py
+++ codes/web/settings.py
@@ -29,6 +29,8 @@ ALLOWED_HOSTS = ['grouptext-django.dreamstate-4-all.org',
                  'localhost']
 
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'grouptext'
+    'grouptext',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'web.urls'
